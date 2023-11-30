a3e8213e4a5bf4e3372107f3de8433efe62eae85
Lucas <lucasvillanuevagmp@gmail.com>
2023-11-30 19:38:10 -0300

    Remove unused import and commented code in login-view.tsx

--- src/sections/login-view.tsx
+++ src/sections/login-view.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import axios from "axios";
 import { object, string } from "yup";
 
 import loginImage from "@/assets/images/login.webp";
@@ -26,8 +25,6 @@ export default function Page() {
     setError(false);
     try {
       await loginSchema.validate(data);
-
-      //Login logic
     } catch (error) {
       setError(error);
     }
@@ -36,23 +33,21 @@ export default function Page() {
   return (
     <main className="w-full bg-martinique-950 h-screen flex items-center justify-center">
       <section className="w-2/5 h-5/6 max-w-md py-10 bg-astral-900 relative flex flex-col items-center justify-center rounded-3xl shadow-xl border-[3px] border-astral-950">
-        <h1
-          className={`text-8xl relative tracking-tighter scale-y-110 -rotate-12 lowercase font-display text-brown-pod-300 stroke title-shadow`}
-        >
+        <h1 className="text-8xl relative tracking-tighter scale-y-110 -rotate-12 lowercase font-display text-brown-pod-300 stroke title-shadow">
           Memealo!
         </h1>
         <form
           className="flex w-full h-4/6 my-3 flex-col items-center justify-center"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <label className={`flex flex-col w-4/6 z-20 text-stroke`}>
+          <label className="flex flex-col w-4/6 z-20 text-stroke">
             <span>Nombre De Usuario:</span>
             <input
               className="my-3 rounded-full text-black py-0.5 px-3 transition-all focus:outline focus:outline-2 focus:outline-astral-950"
               {...register("username", { required: true })}
             />
           </label>
-          <label className={`flex flex-col w-4/6 z-20 text-stroke`}>
+          <label className="flex flex-col w-4/6 z-20 text-stroke">
             Contraseña:
             <input
               className="my-3 rounded-full text-black py-0.5 px-3 transition-all focus:outline focus:outline-2 focus:outline-astral-950"
@@ -60,19 +55,9 @@ export default function Page() {
               {...register("password", { required: true })}
             />
           </label>
-          {/*}
-          <div className="w-4/6 h-14 py-2 rounded-full flex items-center justify-center bg-coral-red-400 z-20">
-            <button
-              type="submit"
-              className="cursor-pointer text-lg w-5/6 h-5/6 relative active:before:shadow-none active:shadow-none before:rounded-xl before:absolute before:top-0 before:left-0 before:w-full before:h-full before:z-10 before:shadow-brown-pod-200 before:shadow-inner rounded-xl shadow-sm shadow-martinique-950 flex items-center justify-center bg-coral-red-400"
-            >
-              Let&apos;s Go!
-            </button>
-          </div>
-          {*/}
           <button
             type="submit"
-            className={`cursor-pointer my-3 text-3xl text-brown-pod-300 stroke-btn title-shadow`}
+            className="cursor-pointer my-3 text-3xl text-brown-pod-300 stroke-btn title-shadow"
           >
             Iniciar Sesion
           </button>
