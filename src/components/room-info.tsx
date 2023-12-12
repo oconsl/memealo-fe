a94ad7dff74d374410e82308864041dd31bfaf7b
Lucas <lucasvillanuevagmp@gmail.com>
2023-12-12 18:00:18 -0300

    Refactor RoomInfo component to use a single description prop and add a countdown timer

--- src/components/room-info.tsx
+++ src/components/room-info.tsx
@@ -1,20 +1,29 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function RoomInfo({
-  topDescription,
-  bottomDescription,
+  description,
+  time,
 }: {
-  topDescription: string;
-  bottomDescription?: string;
+  description: string;
+  time: number;
 }) {
+  const [seconds, setSeconds] = useState(time);
+
+  useEffect(() => {
+    if (seconds > 0) {
+      setTimeout(() => setSeconds(seconds - 1), 1000);
+    }
+  });
+
   return (
     <article className="flex w-96 flex-col">
       <div className="py-6 px-4 w-full h-full bg-martinique-900 mx-3 my-6 rounded-xl text-5xl text-center light-stroke text-shadow">
-        {topDescription}
+        {description}
       </div>
-      {bottomDescription && (
-        <div className="py-6 px-4 w-full h-full bg-martinique-900 mx-3 my-6 rounded-xl text-8xl text-center light-stroke text-shadow">
-          {bottomDescription}
-        </div>
-      )}
+      <div className="py-6 px-4 w-full h-full bg-martinique-900 mx-3 my-6 rounded-xl text-8xl text-center light-stroke text-shadow">
+        {seconds}
+      </div>
     </article>
   );
 }
