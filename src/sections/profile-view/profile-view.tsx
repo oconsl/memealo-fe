425082667b743d3f685bb0cae5190e9ba49bb9c2
Daniel Alejandro Luna <daniel.alejandro.luna032@gmail.com>
2023-12-18 20:44:34 -0300

    Added profile-view of memealo

---
+++ src/sections/profile-view/profile-view.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import { useState } from 'react'
+import ArrowUp from '../../../public/icons/arrow-up.svg'
+import ArrowDown from '../../../public/icons/arrow-down.svg'
+
+
+const properties = {
+    name: "DanielLuna023",
+    division: "Oro II",
+    obtainedCards: "34",
+}
+
+
+
+export default function ProfileView () {
+    const [open, setOpen] = useState(false)
+  
+    return (
+        <section className=" max-w-full min-h-screen bg-martinique-950 pt-10 flex flex-col items-center text-martinique-50" >
+            <div className="w-10/12 h-fit flex flex-col items-center">
+                <h1 className="text-8xl">Perfil</h1>
+                <div className="border-martinique-100 border-4 rounded-lg  w-full h-full shadow-martinique-200 shadow-box flex flex-col gap-3 p-7 text-3xl mb-5" >
+                    
+                    <div className="rounded-lg w-full h-full flex flex-row gap-3 text-5xl" >
+
+                        <img className="border-martinique-800 border-4 rounded-full w-2/12 aspect-square" src="https://pbs.twimg.com/profile_images/1211831363839647744/cEbVQ4qw_400x400.jpg" alt="User Image" />
+
+                        <div className="bg-martinique-700 rounded-lg  w-5/12 min-h-fit flex flex-col items-center justify-center gap-3">
+                            <h2>Usuario:</h2>
+                            <p>{properties.name}</p>
+                        </div>
+
+                        <div className="bg-martinique-700 rounded-lg w-5/12 min-h-fit flex flex-col items-center justify-center gap-3">
+                            <h2>Clasificacion:</h2>
+                            <p className="">{properties.division}</p>
+                        </div>
+                    </div>
+
+                    <div className="bg-martinique-700 rounded-lg w-full h-fit p-5" >
+                        <p>Ultimas Cartas Conseguidas</p>
+                        
+                    </div>
+
+                    <div id='menu' className={`${open && 'h-[700px]'} bg-martinique-700 rounded-lg w-full h-fit p-5 flex  item-start justify-between gap-3 transition-all`}>
+                        <p>Cartas Obtenidas: {properties.obtainedCards}</p>
+                        <button onClick={() => setOpen(!open)} className={`${open && 'bg-up-arrow'} bg-down-arrow w-5 h-5 bg-cover transition-all` }>
+                        </button>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-3 justify-end">
+                        <button className="bg-coral-red-500 rounded-lg p-2 hover:bg-coral-red-400">
+                            Cambiar Avatar
+                        </button>
+                        <button className="bg-coral-red-500 rounded-lg p-2 hover:bg-coral-red-400">
+                            Cambiar Fondo
+                        </button>
+                        <button className="bg-coral-red-500 rounded-lg p-2 hover:bg-coral-red-400">
+                            Cambiar Marco
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,70 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        backgroundImage: {
+          'down-arrow': 'url(../../public/icons/arrow-down.svg)',
+          'up-arrow': 'url(../../public/icons/arrow-up.svg)'
+        },
+
+        colors: {
+          'martinique': {
+            '50': '#f3f5fb',
+            '100': '#e5e8f4',
+            '200': '#d1d8ec',
+            '300': '#b1bedf',
+            '400': '#8b9ccf',
+            '500': '#6f7fc2',
+            '600': '#5c67b4',
+            '700': '#5157a4',
+            '800': '#464887',
+            '900': '#3c3f6c',
+            '950': '#2b2c48',
+        },
+        'brown-pod': {
+            '50': '#fdf9ed',
+            '100': '#f8eecd',
+            '200': '#f1da96',
+            '300': '#eac35f',
+            '400': '#e5ac3a',
+            '500': '#dd8e23',
+            '600': '#c36c1c',
+            '700': '#a34e1a',
+            '800': '#853d1b',
+            '900': '#6d341a',
+            '950': '#401a0a',
+        },
+        'coral-red': {
+            '50': '#fff1f1',
+            '100': '#ffe1e2',
+            '200': '#ffc7c8',
+            '300': '#ffa0a2',
+            '400': '#ff6a6d',
+            '500': '#f83b3f',
+            '600': '#e61c20',
+            '700': '#c11418',
+            '800': '#a01417',
+            '900': '#84181a',
+            '950': '#480708',
+        },
+        'astral': {
+            '50': '#f1f7fa',
+            '100': '#dceaf1',
+            '200': '#bdd7e4',
+            '300': '#8fbad1',
+            '400': '#5a93b6',
+            '500': '#3f789b',
+            '600': '#376283',
+            '700': '#32526c',
+            '800': '#30465a',
+            '900': '#2b3c4e',
+            '950': '#192633',
+        },
       },
+
     },
   },
   plugins: [],
